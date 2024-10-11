d5f0284f6c3c6d5a5cc7f705ed2be891baaab307
Izuna002 <159679683+Izuna002@users.noreply.github.com>
2024-10-11 21:56:03 +0800

    Add files via upload

---
+++ Rodenas/core/dbConfig.php
@@ -0,0 +1,13 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$dbname = "rodenas";
+
+$dsn = "mysql:host={$host};dbname={$dbname}";
+
+$pdo = new PDO(dsn: $dsn, username: $user, password: $password);
+$pdo->exec(statement: "SET time_zone = '+08:00'"); 
+
+?>
---
+++ Rodenas/index.php
@@ -0,0 +1,43 @@
+<?php require_once 'dbconfig.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <style>
+    table, th, td {
+      border: 1px solid black;
+    }
+  </style>
+</head>
+<body>
+
+  <?php
+  $query = "SELECT name, join_date FROM members";
+  $stmt = $pdo->prepare(query: $query);
+  $executeQuery = $stmt->execute();
+
+  if ($executeQuery) {
+    $members = $stmt->fetchAll();
+  } else {
+    echo "Query failed";
+  }
+  ?>
+
+  <table>
+    <tr>
+      <th>Name</th>
+      <th>Join Date</th>
+    </tr>
+    <?php foreach ($members as $row) { ?>
+      <tr>
+        <td><?php echo $row['name']; ?></td>
+        <td><?php echo $row['join_date']; ?></td>
+      </tr>
+    <?php } ?>
+  </table>
+
+</body>
+</html>
